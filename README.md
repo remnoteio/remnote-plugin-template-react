ad07e8d74172421d0b7e81a8a638305c16451396
Hannes Frank <frankhannes@gmail.com>
2022-02-07 19:55:54 +0100

    Remove unnecessary stuff

---
+++ README.md
@@ -0,0 +1,10 @@
+# Template for React-based RemNote Plugins
+
+## Usage
+
+**Prerequisites**: You need to have `git` and `node` installed and some basic understanding of React.
+
+1. Create a new repo based on this template.
+2. Install dependencies with `npm install`.
+3. Start plugin development server with `npm run dev`
+4.
--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
+    <div className="plugin">
+        <p>RemNote Plugin</p>
         <p>
           <a
-            className="App-link"
+            className="link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -29,15 +19,17 @@ function App() {
           </a>
           {' | '}
           <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
+            className="link"
+            href="https://remnote.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Vite Docs
+            RemNote Plugin Docs
           </a>
         </p>
-      </header>
+        <p>
+          <button>Show Toast</button>
+        </p>
     </div>
   )
 }
