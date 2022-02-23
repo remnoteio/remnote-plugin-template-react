622b73d3b4e77db2031acf057a1c7829c6e27b62
Martin Schneider <martin@martins-mbp.myfiosgateway.com>
2022-02-23 02:35:36 -0500

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defaultRemNotePluginViteConfig } from "remnote-plugin-sdk";
 const { resolve } = require("path");
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,
-    strictPort: true,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-      },
-      input: {
-        index: resolve(__dirname, "index.html"),
-        widget1: resolve(__dirname, "widget1.html"),
-      },
-    },
-  },
-});
+export default defineConfig(
+  defaultRemNotePluginViteConfig(require, [react()], ["widget1"])
+);
