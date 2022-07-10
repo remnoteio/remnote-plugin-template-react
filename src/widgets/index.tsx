d9f1ec339f6a934787bf35226500d416fc17a0b0
Hannes Frank <frankhannes@gmail.com>
2022-07-10 18:24:59 +0300

    Adding app styles

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,5 +1,6 @@
 import { declareIndexPlugin, ReactRNPlugin, WidgetLocation } from '@remnote/plugin-sdk';
 import '../style.css';
+import '../App.css';
 
 async function onActivate(plugin: ReactRNPlugin) {
   await plugin.settings.registerStringSetting({
