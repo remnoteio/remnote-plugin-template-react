6aeecf9a4bdce7f00df244c4e1aab47064bcb304
jamesb <experimentallearning0@gmail.com>
2022-08-25 11:39:55 +0100

    Validate manifest on build

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -1,8 +1,8 @@
 import * as fs from 'fs';
 import {exit} from 'process';
-import {parseManifest} from '@remnote/plugin-sdk/dist/validation';
+import {parseManifest} from '@remnote/plugin-sdk';
 
-const MANIFEST = "manifest.json"
+const MANIFEST = "public/manifest.json"
 const README = "README.md"
 
 const fileExists = (fp: string): boolean => {
@@ -27,18 +27,27 @@ const tryParseJson = (str: string) => {
 const parseManifestFile = (fp: string) => {
   const fileText = fs.readFileSync(fp, { encoding: "utf-8" })
   const json = tryParseJson(fileText)
-  return json && parseManifest(json)
+  if (!json) {
+    return {
+      success: false,
+      error: ["Failed to parse JSON. Are you sure the manifest.json file contains valid JSON?"],
+    }
+  }
+  else {
+    return parseManifest(json)
+  }
 }
 
 const main = () => {
   if (!fileExists(README)) {
-    console.log("You must include a README.md file in the project root directory describing what the plugin does and how to use it.");
+    console.log("You must include a README.md file in the project root directory describing what the plugin does and how to use it.\n");
     exit(1);
   }
 
   const parsedManifest = parseManifestFile(MANIFEST);
-  if (parsedManifest.error) {
-    console.log("Invalid manifest. Please correct the following errors and ")
+  if (parsedManifest.success == false) {
+    console.log("Invalid manifest. Please correct the following errors and rebuild")
+    console.log(parsedManifest.error.map(e => '- ' + e).join(""))
     exit(1)
   }
 }
